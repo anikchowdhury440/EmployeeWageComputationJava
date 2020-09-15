ab8245ff00b9abe229496727b81e3259eab5b4a3
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-15 12:45:32 +0530

    Solved using Switch Case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -8,17 +8,15 @@ public class EmpWageComputation
 		int empHrs, empWage;
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		if ( empCheck == IS_FULL_TIME )
-		{
-			empHrs=8;
-		}
-		else if ( empCheck == IS_PART_TIME )
-		{
-			empHrs=4;
-		}
-		else
-		{
-			empHrs=0;
+		switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Eage: " + empWage);
