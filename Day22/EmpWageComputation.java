5c70d3579057ac2da195719b751fc4136b26956f
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-18 15:14:58 +0530

    Employee Wage multiple comapny procedural

---
+++ Day22/EmpWageComputation.java
@@ -0,0 +1,37 @@
+public class EmpWageComputation
+{
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+
+	public static int computeEmpWage(String company, int empRatePerHour, int numWorkingDays, int maxWorkingHrs)
+	{
+		int empHrs, totalEmpWage, totalEmpHrs=0, totalWorkingDays=0;
+                while ( totalEmpHrs <= maxWorkingHrs && totalWorkingDays < numWorkingDays )
+                {
+                        totalWorkingDays++;
+                        int empCheck=(int)Math.floor(Math.random()*10)%3;
+                        switch (empCheck)
+                        {
+                                case IS_FULL_TIME:
+                                        empHrs=8;
+                                        break;
+                                case IS_PART_TIME:
+                                        empHrs=4;
+                                        break;
+                                default:
+                                        empHrs=0;
+                        }
+                        totalEmpHrs += empHrs;
+                        System.out.println("Day" + totalWorkingDays + " Emp Hr: " +empHrs);
+                }
+                totalEmpWage = totalEmpHrs * empRatePerHour;
+                System.out.println("Total Emp Wage for Comapany: " + company + " is " + totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args)
+	{
+		computeEmpWage("DMart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
+	}
+}
